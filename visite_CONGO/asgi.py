96ac5fa4f76271b302fc6c9bfcad506604693a1d
winterkalala <ephraimekalala20@gmail.com>
2026-05-12 00:57:51 +0100

    new mise à jour

---
+++ visite_CONGO/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for visite_CONGO project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'visite_CONGO.settings')
+
+application = get_asgi_application()
